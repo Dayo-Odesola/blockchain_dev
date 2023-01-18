b63966e48b59475d88f7d124600eb06023f41ab9
dayo-odesola <dayoodesola2001@gmail.com>
2023-01-18 19:32:39 +0100

    4. Token Balances

--- test/Token.js
+++ test/Token.js
@@ -7,20 +7,24 @@ const tokens = (n) => {
 
 describe('Token', () => {
 
-  let token;
-
+  let token, accounts, deployer
+  
   beforeEach(async () => {
     // Fetch token from Blockchain
     const Token = await ethers.getContractFactory('Token')
      token = await Token.deploy('Dapp University', 'DAPP', '1000000')  
+
+    accounts = await ethers.getSigners()
+    deployer = accounts[0]
+
   })
 
   describe('Deployment', () => {
 
-    const name = 'Dapp University';
+    const name = 'Dapp University'
     const symbol = 'DAPP'
     const decimals = '18'
-    const totalSupply = '1000000'
+    const totalSupply = tokens('1000000')
 
     it('has correct name ', async () => {
       expect(await token.name()).to.equal(name)
@@ -35,12 +39,13 @@ describe('Token', () => {
     })
   
     it('has correct total supply', async () => {
-      expect(await token.totalSupply()).to.equal(tokens(totalSupply))
+      expect(await token.totalSupply()).to.equal(totalSupply)
+    })
+
+
+    it('assigns total supply to deployer', async () => {
+      expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
     })
   })
-
-
-  
-
 
 })
